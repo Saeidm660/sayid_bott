a46dbfddaea3bcf94ac9cbb70a1849e8617593dd
Saeidm660 <48598439+Saeidm660@users.noreply.github.com>
2021-12-25 21:40:09 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import telebot 
+import os
+import telegram_send
+import url
+import time
+
+
+
+
+TOKEN = os.environ['TOKEN']
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(msg):
+  bot.reply_to(msg, 'سلام حشری')
+
+file = open("url.txt","r")
+lines = file.readlines()
+
+for i in lines:
+  bot.send_photo(-1001208760301, i)
+  time.sleep(60)
+
+bot.polling()
