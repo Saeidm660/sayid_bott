7cd22069371c0ff62a4335a2820b1575f51b1bca
Saeidm660 <48598439+Saeidm660@users.noreply.github.com>
2021-12-26 12:04:59 +0330

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(msg):
-  bot.reply_to(msg, 'سلام حشری')
+  bot.reply_to(msg, 'سلام ')
 
 file = open("url.txt","r")
 lines = file.readlines()
 
 for i in lines:
-  bot.send_photo(-332488397, i)
+  bot.send_photo(-1001208760301, i)
   print("send" ,i)
-  time.sleep(60)
+  time.sleep(300)
 
 bot.polling()
